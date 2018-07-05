ba38d320864fad3d38e137fc7932a1c2b2ee2b42
José Seie <joseeugenioseie@hotmail.com>
2018-07-05 15:48:55 +0200

    Adicao dos scripts e Css

---
+++ src/assets/js/script.js
@@ -0,0 +1,1 @@
+alert('Test good seie')
---
+++ src/components/seie-cv/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+      <header></header>
+      <overview></overview>
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+import OverView from './OverView.vue'
+export default {
+  components: {
+    'header': Header,
+    'overview': OverView
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style></style>
